0a16745461ac61c435df5d2473de72de81e35856
Frère Jacques <zyb@zjex.com>
2017-06-29 11:39:03 +0800

    !

--- run-all.bat
+++ run-all.bat
@@ -1,42 +1,61 @@
 title run-all
 cd config
 start run.bat  
+echo off 
 ping 127.0.1 -n 10
+echo on  
 cd ..
 
 cd registry
 start run.bat  
+echo off 
 ping 127.0.1 -n 15
+echo on  
 cd ..
 
 cd auth-service
 start run.bat  
+echo off 
 ping 127.0.1 -n 35
+echo on  
 cd ..
 
 cd account-service
 start run.bat  
+echo off 
 ping 127.0.1 -n 35
+echo on  
 cd ..
 
 cd gateway
 start run.bat  
+echo off 
 ping 127.0.1 -n 35
+echo on  
 cd ..
 
 cd monitoring
 start run.bat  
+echo off 
 ping 127.0.1 -n 20
+echo on  
 cd ..
 
 cd notification-service
 start run.bat  
+echo off 
 ping 127.0.1 -n 20
+echo on  
 cd ..
 
 cd statistics-service
 start run.bat  
+echo off 
 ping 127.0.1 -n 20
+echo on  
 cd ..
 
-echo "done"
+echo "done! exit in 20s "
+echo off 
+ping 127.0.1 -n 20
+echo on 
